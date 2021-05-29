72694b6244fa228f72db6af550e0f4022d8c9cae
Crowdbotics <team@crowdbotics.com>
2021-05-29 05:00:51 +0000

    Update models

--- covid19_counselling_27544/wsgi.py
+++ covid19_counselling_27544/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'covid19_counselling_27544.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "covid19_counselling_27544.settings")
 
 application = get_wsgi_application()
--- home/models.py
+++ home/models.py
@@ -1,1 +1,38 @@
+from django.conf import settings
 from django.db import models
+
+
+class Patient(models.Model):
+    "Generated Model"
+    patientname = models.TextField(
+        blank=True,
+    )
+    age = models.IntegerField(
+        null=True,
+        blank=True,
+    )
+    address = models.TextField(
+        null=True,
+        blank=True,
+    )
+    phonenumber = models.CharField(
+        max_length=256,
+        null=True,
+        blank=True,
+    )
+    problem = models.TextField(
+        null=True,
+        blank=True,
+    )
+    o2level = models.IntegerField(
+        null=True,
+        blank=True,
+    )
+    doctor = models.TextField(
+        null=True,
+        blank=True,
+    )
+    problemsice = models.DateTimeField(
+        null=True,
+        blank=True,
+    )
